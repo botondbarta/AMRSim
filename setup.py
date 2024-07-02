481dd6a40ca95c52ec8756b91a8296568cf1a038
Botond Barta <b.botond25@gmail.com>
2024-07-02 11:00:45 +0200

    updated packages to work with cuda 12.4

--- setup.py
+++ setup.py
@@ -12,16 +12,16 @@ setup(
     install_requires=[
         'setuptools~=69.5.1',
         'amrlib==0.8.0',
+        'nltk==3.7',
         'numpy==1.26.4',
         'pandas==2.2.2',
         'penman==1.3.0',
-        'torch==1.12.0',
+        'torch==2.3.1',
         'torch_geometric==2.1.0',
-        'torch_sparse==0.6.15',
+        'torch_scatter==2.1.2',
+        'torch_sparse==0.6.18',
         'transformers==4.24.0',
         'networkx==2.6.3',
         'unidecode==1.3.6',
-        'nltk==3.7',
-        'torch_scatter==2.0.9',
     ]
 )
